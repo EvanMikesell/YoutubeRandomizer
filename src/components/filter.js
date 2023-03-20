4d8e7a05c5cc45ed2d53bdc3db3b7d059a616805
Evan Mikesell <mikesellevan@gmail.com>
2023-03-20 02:03:30 -0700

    Update filter.js to current year

--- src/components/filter.js
+++ src/components/filter.js
@@ -74,6 +74,9 @@ class Filter extends Component {
             <option value="2018">2018</option>
             <option value="2019">2019</option>
             <option value="2020">2020</option>
+            <option value="2020">2021</option>
+            <option value="2020">2022</option>
+            <option value="2020">2023</option>
           </select>
         </div>
       </form>
