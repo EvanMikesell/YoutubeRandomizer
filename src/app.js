81a1385eac30310f9209b761a5fc04eb9bea147a
Evan Mikesell <mikesellevan@gmail.com>
2023-04-21 16:58:00 -0700

    updating search word list creation
    updating the way the search words are created so that the list is only called once, instead of every time the getVideo function is called.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import MiddleRow from "./components/middleRow.js";
 import EmbeddedVideo from "./components/embeddedVideo.js";
 
 //map containing categories and their ID values (for example, the ID for Film & Action is 1)
-var catagoriesMap = new Map();
+const catagoriesMap = new Map();
 catagoriesMap.set("No category", 0);
 catagoriesMap.set("Film & Animation", 1);
 catagoriesMap.set("Autos & Vehicles", 2);
@@ -33,13 +33,18 @@ class App extends Component {
       searchWords: [],
       category: 0,
       length: "any",
-      year: 0,
+      year: 0
     };
+  }
+
+  async componentDidMount() {
+    console.log("component did mount");
+    await this.createWordList();
   }
 
   //getting values from our filter
   updateFilters(categoryName, duration, selectedYear) {
-    var categoryNumber = catagoriesMap.get(categoryName);
+    let categoryNumber = catagoriesMap.get(categoryName);
     this.setState({ category: categoryNumber}); 
     this.setState({ length: duration}); 
     this.setState({ year: selectedYear}); 
@@ -49,36 +54,22 @@ class App extends Component {
   };
 
   async getVideo() {
-    var keywords = await this.createWordList();
-    var keyword = keywords[Math.floor(Math.random() * keywords.length + 1)];
+    let keyword = this.state.searchWords[Math.floor(Math.random() * this.state.searchWords.length + 1)];
     console.log("Chosen Keyword: " + keyword);
     const youtubeAPIKey = "AIzaSyAWwZk3tMo_OZp0JZ9Y17oxykJpng6Q-nY";
 
     //api url before adding our values from the filters
-    var baseApiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&key=" + youtubeAPIKey;
+    let baseApiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&key=" + youtubeAPIKey;
     
     //filter by a category
-    if (this.state.category !== 0){
-      baseApiUrl += "&videoCategoryId=" + this.state.category;
-    }
-    
-    //filter by length
-    if (this.state.length !== "any"){
-      baseApiUrl += "&videoDuration=" + this.state.length;
-    }
-   
-    //filter by year
-    if (this.state.year !== 0){
-      baseApiUrl += "&publishedBefore=" + this.state.year + "-12-31T00:00:00.0Z" + 
-                    "&publishedAfter=" + this.state.year + "-01-01T00:00:00.0Z";
-    }
+    baseApiUrl = this.applyFilters(baseApiUrl);
 
     //apply our keyword in a new variable
-    var apiUrlWithKeyword = baseApiUrl + "&q=" + keyword
+    let apiUrlWithKeyword = baseApiUrl + "&q=" + keyword
 
     let res = await fetch(apiUrlWithKeyword);
     let data = await res.json(); 
-    var searchLength;
+    let searchLength = 0;
 
     //checking if api quota has been exceeded, searchlength will be undefined
     try{
@@ -102,25 +93,40 @@ class App extends Component {
         alert("Found a video, but didn't use keyword");
       }
     }
-    var idIndex = Math.floor(Math.random() * searchLength);
-    console.log("LENGTH: " + data.items.length);
-    var randomURL = "https://www.youtube.com/embed/" + data.items[idIndex].id.videoId;
-    console.log("Our URL: " + randomURL);
-    console.log(data);
+
+    let idIndex = Math.floor(Math.random() * searchLength);
+    let randomURL = "https://www.youtube.com/embed/" + data.items[idIndex].id.videoId;
     this.setState({ videoURL: randomURL });
     return randomURL;
   }
 
-  //creating random word list that we will select a video out of
-  async createWordList() {
-    var words = [];
-    let res = await fetch("/common-words.json");
-    let data = await res.json();
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-      words.push(data[i]);
+  applyFilters(baseApiUrl) {
+    //filter by category, length, and year
+    if (this.state.category !== 0) {
+      baseApiUrl += "&videoCategoryId=" + this.state.category;
     }
-    return words;
+
+    if (this.state.length !== "any") {
+      baseApiUrl += "&videoDuration=" + this.state.length;
+    }
+
+    if (this.state.year !== 0) {
+      baseApiUrl += "&publishedBefore=" + this.state.year + "-12-31T00:00:00.0Z" +
+        "&publishedAfter=" + this.state.year + "-01-01T00:00:00.0Z";
+    }
+    return baseApiUrl;
+  }
+
+  //creating random word list that we will use to search for a video
+  async createWordList() {
+    try {
+      const response = await fetch("/common-words.json");
+      const data = await response.json();
+      console.log(data);
+      this.setState({ searchWords: data });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   getRandomVideo = () => {
